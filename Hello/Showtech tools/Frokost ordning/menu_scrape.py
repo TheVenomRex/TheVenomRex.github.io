6c96790d47b01a903303d38e18ef2121363eb0f0
Jakob B. L. Dalgaard <thevenomrex@gmail.com>
2024-04-15 17:04:34 +0200

    further list manip
    needs 2(?) steps to finish

--- Hello/Showtech tools/Frokost ordning/menu_scrape.py
+++ Hello/Showtech tools/Frokost ordning/menu_scrape.py
@@ -19,15 +19,59 @@ snips = open("html menu 15-16 v2.txt", "r").read()
 ##subSnip = match_menuPattern.group()
 listPattern = "<p.*Salater.*</p>"
 subSnip = re.findall(listPattern,snips)
-subSnipList = list(subSnip)
 #subSnip = re.sub("<.*?>","",subSnip) 
 #cleaning the list
 #for x in subSnip:
     #x = re.sub("&amp;","&",x)
     #x = re.sub("</p>","\n",x)
-[re.sub("<.*?>", " ", subSnipList[i]) for i in subSnipList]
+#[re.sub("<.*?>", " ", i) for i in subSnipList]
 menuSnip = open("menu snip uge 16.txt","a")
-for x in subSnipList:
-    menuSnip.write(x)
-    menuSnip.write("\n")
+testMenu = []
+for x in subSnip[5:]:
+    x = re.sub("&amp;",r"\&",x)
+    x = re.sub("<br />","\n",x)
+    x = re.sub("<.*?>","",x)
+    #x = re.sub("Salater","",x)
+    #x = re.sub("Pålæg","",x)
+    #x = re.sub("Varm ret","",x)
+    #x = re.sub("/ minus gris","",x)
+    #x = re.sub("/ vegetar","",x)    
+#    menuSnip.write(x)
+#    menuSnip.write("\n")
+    testMenu.extend(re.findall(r"\n(.+)",x))
+menuSnip.write("\n \n")
+
+#testMenu.extend(re.findall("^.+.*$",subSnip))
+
+## time to clean
+#friday
+del testMenu[-2]
+del testMenu[-3]
+del testMenu[-5:-3]
+del testMenu[-8:-6]
+#thursday
+del testMenu[-10]
+del testMenu[-11]
+del testMenu[-13:-11]
+del testMenu[-16:-14]
+#wednesday
+del testMenu[-18]
+del testMenu[-19]
+del testMenu[-20]
+del testMenu[-22:-20]
+del testMenu[-25:-23]
+#tuesday
+del testMenu[-27]
+del testMenu[-28]
+del testMenu[-30:-28]
+del testMenu[-33:-31]
+#thursday
+del testMenu[-35]
+del testMenu[-36]
+del testMenu[-38:-36]
+del testMenu[-41:-39]
+#del testMenu[2:3]
+for i in testMenu:
+    menuSnip.write(i + "\n")
 menuSnip.close
+
