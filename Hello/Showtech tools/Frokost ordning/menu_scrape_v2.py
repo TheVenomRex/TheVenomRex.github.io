f34edee60e4fe9430885dfa5fc27da6960b2faa6
Jakob B. L. Dalgaard <thevenomrex@gmail.com>
2024-07-02 11:45:21 +0200

    week 28

--- Hello/Showtech tools/Frokost ordning/menu_scrape_v2.py
+++ Hello/Showtech tools/Frokost ordning/menu_scrape_v2.py
@@ -378,7 +378,7 @@ overwrite_wednesday()
 overwrite_thursday()
 overwrite_friday()
 
-with open("template 28.txt", "a") as file: # Change week number to the correct week
+with open("template 29.txt", "a") as file: # Change week number to the correct week
     file.writelines(template)
 
 with open("menu snip.txt", "a") as file:
